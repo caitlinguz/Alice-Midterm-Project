15e6bb0966f46e83838968b62148d53e0d0ced69
Caitlin Guzman <cg1656@nyu.edu>
2015-03-02 22:52:05 -0500

    Added invisible wall paths, flower, blocked passage. To do: work on scripts for flower with reaction scripts on other things, fill out path beyond lovers, smooth out Puck's jump

---
+++ Assets/Scripts/FlowerPower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerPower : MonoBehaviour {
+
+	public bool hasFlower = false;
+	public Rigidbody player;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if player 
+
+	}
+}
---
+++ Assets/Scripts/PhysicsControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsControl : MonoBehaviour {
+
+	public float speed = 5f;
+	public float turnspeed = 45f;
+	public float jumpheight = 10f;
+	public float jumpspeed = 5f;
+	public Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody>();
+	}
+	
+	// FixedUpdate is called on a fixed interval for every physics frame
+	// you'll want to use FixedUpdate for doing anything with physics
+	void FixedUpdate () {
+
+		// INPUT AXES: a virtual joystick that returns a float from -1 to 1
+
+		player.AddForce ( transform.forward * speed * Input.GetAxis ( "Vertical"));
+
+		// comment out sideways movement
+		//player.AddForce ( transform.right * speed * Input.GetAxis ( "Horizontal"));
+
+		// TURNING
+		transform.Rotate ( 0f, Input.GetAxis ("Horizontal") * turnspeed, 0f);
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			transform.position += new Vector3 (0f,jumpheight,0f) * Time.deltaTime * jumpspeed;
+		}
+	}
+}
